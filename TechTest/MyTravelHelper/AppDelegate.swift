31a472586a86f09d5325bcd042fbd81cbc66994a
Rahul Jain <rahul.jain@harman.com>
2021-01-23 14:00:17 +0530

    Fixed -White screen issue while launching app

--- TechTest/MyTravelHelper/AppDelegate.swift
+++ TechTest/MyTravelHelper/AppDelegate.swift
@@ -11,9 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var navigation: UINavigationController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let notice = SearchTrainRouter.createModule()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        navigation = UINavigationController(rootViewController: notice)
+        window?.rootViewController = navigation
+        window?.makeKeyAndVisible()
         return false
     }
 }
